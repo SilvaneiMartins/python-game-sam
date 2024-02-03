6b386127555b4a9df45eaf277b8f7d2dd9d2ebf3
Silvanei  Martins <silvaneimartins@Silvaneis-MacBook-Air.local>
2024-02-03 18:11:02 -0400

    gerador de background

--- tutorial.py
+++ tutorial.py
@@ -10,8 +10,7 @@ from os.path import isfile, join
 pygame.init()
 pygame.display.set_caption("Platformer Sam Gamer")
 
-# Defina as variáveis
-BG_COLOR = (255, 255, 255)
+# Defina as variáveis globais
 WIDTH, HEIGHT = 1000, 700
 FPS = 60
 PLAY_VEL = 5
@@ -19,8 +18,30 @@ PLAY_VEL = 5
 # Defina a largura e a altura da tela [width, height]
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# Pega o background
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+# Desenha o background
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
+# Função principal
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Blue.png")
 
     run = True
     while run:
@@ -31,8 +52,11 @@ def main(window):
                 run = False
                 break
 
+        draw(window, background, bg_image)
+
     pygame.quit()
     quit()
 
+# Inicializa o jogo
 if __name__ == "__main__":
     main(window)
